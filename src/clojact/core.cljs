11f9de99add732e706bda5170f699110ace6d587
Scott Sinclair <scottysinclair@gmail.com>
2018-11-14 16:11:14 +0100

    reducing code.

--- src/clojact/core.cljs
+++ src/clojact/core.cljs
@@ -23,21 +23,6 @@
                                                           :primary2Color (color "green")})                                      clj->js))})
 
 
-;; -- Domino 1 - Event Dispatch -----------------------------------------------
-(defn dispatch-timer-event
-  []
-  (let [now (js/Date.)]
-    (rf/dispatch [:timer now])))  ;; <-- dispatch used
-
-;; Call the dispatching function every second.
-;; `defonce` is like `def` but it ensures only one instance is ever
-;; created in the face of figwheel hot-reloading of this file.
-(defonce do-timer (js/setInterval dispatch-timer-event 1000))
-
-
-;;
-;;;helper methods to update the app-state
-;;
 ;updates a property of a booking
 (defn updateBooking
   [db booking prop-key prop-value]
@@ -46,7 +31,7 @@
 
 (defn pap [x] (println x) x) 
 
-(defn category-by-id
+(defn get-category-by-id
   [db id]
   (some #(when (= id (:id %)) %) 
         (get db :all-categories)))
@@ -59,6 +44,7 @@
     {:time (js/Date.)         ;; What it returns becomes the new application state
      :time-color "#f88"
      :drawer-open false
+     :selected-category {:id 2 :name "Rent"}
      :all-categories (vector {:id 1 :name "Food"} {:id 2 :name "Rent"} {:id 3 :name "Lunch"})
      :bookings (vector 
       {:id 1
@@ -72,19 +58,8 @@
        :comment "...."
        :amount "820"
       })}))
+
           ;; so the application state will initially be a map with two keys
-
-(rf/reg-event-db                ;; usage:  (dispatch [:time-color-change 34562])
-  :time-color-change            ;; dispatched when the user enters a new colour into the UI text field
-  (fn [db [_ new-color-value]]  ;; -db event handlers given 2 parameters:  current application state and event (a vector)
-    (assoc db :time-color new-color-value)))   ;; compute and return the new application state
-
-(rf/reg-event-db                 ;; usage:  (dispatch [:timer a-js-Date])
-  :timer                         ;; every second an event of this kind will be dispatched
-  (fn [db [_ new-time]]          ;; note how the 2nd parameter is destructured to obtain the data value
-    (assoc db :time new-time)))  ;; compute and return the new application state
-
-
 (rf/reg-event-db                ;;the drawer open state is switched.
    :toggle-app-drawer
    (fn [db _]
@@ -92,46 +67,15 @@
    ))
 
 (rf/reg-event-db
-    :booking-comment-change
+    :booking-change
    (fn [db event]
-     (let [booking (get event 1)
-           new-value (get event 2)]
-        (updateBooking db booking :comment new-value))))
-
-(rf/reg-event-db
-    :booking-amount-change
-   (fn [db event]
-     (let [booking (get event 1)
-           new-value (get event 2)]
-        (updateBooking db booking :amount new-value))))
-
-(rf/reg-event-db
-    :booking-date-change
-   (fn [db event]
-     (let [booking (get event 1)
-           new-value (get event 2)]
-        (updateBooking db booking :date new-value))))
-
-(rf/reg-event-db
-    :booking-category-change
-   (fn [db event]
-     (let [booking (get event 1)
-           new-value (get event 2)]
-       (println "::::::::" booking "jjjjj" new-value)
-        (updateBooking db booking :category (category-by-id db new-value)))))
-
+     (let [booking (get event 1) ;the booking which is changing
+           field   (get event 2) ; the booking field which is changing
+           event-value (get event 3) ; the value from the ui compponent
+           lookup-funct (or (get event 4) (fn [db value] value))] ;optional lookup conversion.
+        (updateBooking db booking field (lookup-funct db event-value)))))
 
 ;; -- Domino 4 - Query  -------------------------------------------------------
-(rf/reg-sub
-  :time
-  (fn [db _]     ;; db is current app state. 2nd unused param is query vector
-    (:time db))) ;; return a query computation over the application state
-
-(rf/reg-sub
-  :time-color
-  (fn [db _]
-    (:time-color db)))
-
 (rf/reg-sub
   :drawer-open
   (fn [db _]
@@ -147,62 +91,50 @@
   (fn [db _]
     (:all-categories db)))
 
+(rf/reg-sub
+  :selected-category
+  (fn [db _]
+    (:selected-category db)))
+
 ;; -- Domino 5 - View Functions ----------------------------------------------
 
-(defn clock
-  []
-  [:div.example-clock
-   {:style {:color @(rf/subscribe [:time-color])}}
-   (-> @(rf/subscribe [:time])
-       .toTimeString
-       first)])
 
-(defn color-input
-  []
-  [:div.color-input
-   "Time color: "
-   [:input {:type "text"
-            :value @(rf/subscribe [:time-color])
-            :on-change #(rf/dispatch [:time-color-change (-> % .-target .-value)])}]])  ;; <---
 
 (defn category-select-field
   [booking]
   (let [cat-list @(rf/subscribe [:all-categories])
         category (get booking :category)]
 	  [ui/select { :value (:id category) 
-                 :onChange #(rf/dispatch [:booking-category-change booking (-> % .-target .-value)])  }
+                 :onChange #(rf/dispatch [:booking-change booking :category (-> % .-target .-value) get-category-by-id])  }
     (for [cat  cat-list]
-	   [ui/menu-item {
-                   :key (:id cat) 
-                   :value (:id cat)
-                   }(:name cat)])]))
+	   [ui/menu-item { :key (:id cat)  :value (:id cat) }(:name cat)])]))
 
 
 (defn booking-table-row
    [booking]
 ;;   (println booking)
-   [ui/table-row {:key (:id booking)}
+   [ui/table-row {:key (:id booking) :className "highlighted-row"}
 		  [ui/table-cell {:key "date"} [ui/text-field {:value (get booking :date) 
-                                                 :onChange #(rf/dispatch [:booking-date-change booking (-> % .-target .-value)]) }]]
+                                                 :onChange #(rf/dispatch [:booking-change booking :date (-> % .-target .-value)]) }]]
 		  [ui/table-cell {:key "cat"} [category-select-field booking]]
 		  [ui/table-cell {:key "com"} [ui/text-field {:value (get booking :comment)
-                                  :onChange #(rf/dispatch [:booking-comment-change booking (-> % .-target .-value)]) }]]
+                                  :onChange #(rf/dispatch [:booking-change booking :comment (-> % .-target .-value)]) }]]
 		  [ui/table-cell {:key "amount"} [ui/text-field {:value (get booking :amount) 
-                                     :onChange #(rf/dispatch [:booking-amount-change booking (-> % .-target .-value)]) }]]
+                                     :onChange #(rf/dispatch [:booking-change booking :amount  (-> % .-target .-value)]) }]]
 	  ])
-
 
 (defn booking-table
   []
   (let [bookings @(rf/subscribe [:bookings])]
-	  [ui/paper {:key "booking-table" :class "booking-table"}
+	  [ui/paper {:key "booking-table"}
 	  [ui/table
 	   [ui/table-head
-		      [ui/table-cell {:key "date"} "Date"]
+	   [ui/table-row
+		      [ui/table-cell {:key "date"}"Date"]
 		      [ui/table-cell {:key "cat"} "Category"]
 		      [ui/table-cell {:key "com"} "Comment"]
 		      [ui/table-cell {:key "amount"} "Amount"]
-	    ]
+	    ]]
 	    [ui/table-body
         (for [booking bookings]
             [booking-table-row booking]
@@ -214,9 +146,10 @@
   [ui/paper {:key "cat-table" :class "category-table"}
   [ui/table
    [ui/table-head 
-	      [ui/table-cell {:key "cat"} "Category"]
-	      [ui/table-cell {:key "tot"} "Total"]
-    ]
+   [ui/table-row
+	     [ui/table-cell {:key "cat"} "Category"]
+	     [ui/table-cell {:key "tot"} "Total"]
+   ]]
     [ui/table-body
      [ui/table-row
 	     [ui/table-cell {:key 1} "Food"]
